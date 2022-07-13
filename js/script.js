0df1961d14f2dfbea2aaa68263eff37a5b015762
Roberto Blanco <86691875+RobertoBlanco20@users.noreply.github.com>
2022-07-13 13:15:46 -0300

    adding html through the button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,130 @@
-const input = document.getElementById('#input');
 
-console.log(input)
+/* VARIABLES */
+const divFather = document.querySelector('.div-father');
+const btnAdd = document.querySelector('#add');
+const mainDiv = document.querySelector('#second-container');
+const input = document.querySelector('#input');
+const emptyTasks = document.querySelector('.empty');
+const reset = document.querySelector('#task-delete');
 
-eventlisteners();
-function eventlisteners(){
-    input.addEventListener('click', formValue)
+console.log(reset);
+
+
+let texto;
+let tasks = [];
+
+/* EVENTLISTENERS */
+cargarListeners ()
+function cargarListeners (){
+
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelectorAll('input[type=text]').forEach( node => node.addEventListener('keypress', e => {
+          if(e.keyCode == 13) {
+            e.preventDefault();
+          }
+        }))
+      });
+    
+    
+    divFather.children[0].addEventListener('blur', readTask);
+    btnAdd.addEventListener('click', createTask);
+
+/*     reset.addEventListener('click', deleteElement); */
+    
 }
 
-
-function formValue(e){
+/* FUNCIONES */
+function readTask(e) {
     e.preventDefault();
-    console.log(e.target.value);
+    
+    texto = e.target.value;
+
+    tasks = [...tasks, texto]
+    console.log(tasks);
 }
 
-console.log('hola')
+
+
+function createTask(e){
+    e.preventDefault();
+
+   
+
+    /* Crear elementos */
+    const p = document.createElement('p');
+
+    const divCheck = document.createElement('div');
+    const aCheck = document.createElement('a');
+    const iCheck = document.createElement('i');
+
+    const divTrash = document.createElement('div');
+    const iTrash = document.createElement('i');
+    const aTrash = document.createElement('a');
+
+    const secondDiv = document.createElement('div');
+
+    /* Clases textos y estilos */
+
+
+    p.textContent = texto;
+    p.classList.add('col-9', 'col-lg-10', 'mt-1');
+
+    divCheck.classList.add('child-div-icon', 'col-1', 'btn');
+    aCheck.setAttribute('href', '#');
+    iCheck.classList.add('check', 'fa-solid', 'fa-check', 'bg-success');
+   
+    divTrash.classList.add('col-1', 'btn');
+    aTrash.setAttribute('href', '#');
+    aTrash.setAttribute('id', 'task-delente');
+    iTrash.classList.add('trash', 'fa-solid', 'fa-trash', 'bg-danger');
+
+    secondDiv.classList.add('row', 'border', 'border-5');
+   
+    /* Insertar HTML */
+    aCheck.appendChild(iCheck);
+    divCheck.appendChild(aCheck);
+
+    
+    aTrash.appendChild(iTrash);
+    divTrash.appendChild(aTrash);
+
+    
+    secondDiv.appendChild(p);
+    secondDiv.appendChild(divCheck);
+    secondDiv.appendChild(divTrash);
+
+    mainDiv.appendChild(secondDiv);
+
+
+    clearForm();
+    emptyMain();
+}
+
+function clearForm() {
+    divFather.children[0].value = ''
+}
+
+
+/* Empty main */
+
+function emptyMain() {
+    if(tasks.length < 0){
+        emptyTasks.classList.remove('d-none');
+    }
+    if(tasks.length >= 0){
+        emptyTasks.classList.add('d-none');
+    }
+}
+
+
+function limpiarHTML(){
+    mainDiv.innerHTML = '';
+}
+
+
+
+
+function deleteElement(e){
+    console.log('Eliminar')
+
+}
