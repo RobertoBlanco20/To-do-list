82dad3a53558b100771ff855a06569e2a44fac06
Roberto Blanco <86691875+RobertoBlanco20@users.noreply.github.com>
2022-07-19 09:48:39 -0300

    to do list project

--- js/script.js
+++ js/script.js
@@ -1,130 +1,76 @@
+const input = document.querySelector("input");
+const addBtn = document.querySelector("#add");
+const ul = document.querySelector("ul");
+const li = document.querySelector("li")
+const empty = document.querySelector(".empty");
 
-/* VARIABLES */
-const divFather = document.querySelector('.div-father');
-const btnAdd = document.querySelector('#add');
-const mainDiv = document.querySelector('#second-container');
-const input = document.querySelector('#input');
-const emptyTasks = document.querySelector('.empty');
-const reset = document.querySelector('#task-delete');
-
-console.log(reset);
-
-
-let texto;
-let tasks = [];
-
-/* EVENTLISTENERS */
-cargarListeners ()
-function cargarListeners (){
-
-    document.addEventListener('DOMContentLoaded', () => {
-        document.querySelectorAll('input[type=text]').forEach( node => node.addEventListener('keypress', e => {
-          if(e.keyCode == 13) {
-            e.preventDefault();
-          }
-        }))
-      });
-    
-    
-    divFather.children[0].addEventListener('blur', readTask);
-    btnAdd.addEventListener('click', createTask);
-
-/*     reset.addEventListener('click', deleteElement); */
-    
+addEventListeners()
+function addEventListeners(){
+    addBtn.addEventListener('click', createTask);
 }
-
-/* FUNCIONES */
-function readTask(e) {
-    e.preventDefault();
-    
-    texto = e.target.value;
-
-    tasks = [...tasks, texto]
-    console.log(tasks);
-}
-
 
 
 function createTask(e){
     e.preventDefault();
 
-   
+    const text = input.value;
 
-    /* Crear elementos */
-    const p = document.createElement('p');
-
-    const divCheck = document.createElement('div');
-    const aCheck = document.createElement('a');
-    const iCheck = document.createElement('i');
-
-    const divTrash = document.createElement('div');
-    const iTrash = document.createElement('i');
-    const aTrash = document.createElement('a');
-
-    const secondDiv = document.createElement('div');
-
-    /* Clases textos y estilos */
-
-
-    p.textContent = texto;
-    p.classList.add('col-9', 'col-lg-10', 'mt-1');
-
-    divCheck.classList.add('child-div-icon', 'col-1', 'btn');
-    aCheck.setAttribute('href', '#');
-    iCheck.classList.add('check', 'fa-solid', 'fa-check', 'bg-success');
-   
-    divTrash.classList.add('col-1', 'btn');
-    aTrash.setAttribute('href', '#');
-    aTrash.setAttribute('id', 'task-delente');
-    iTrash.classList.add('trash', 'fa-solid', 'fa-trash', 'bg-danger');
-
-    secondDiv.classList.add('row', 'border', 'border-5');
-   
-    /* Insertar HTML */
-    aCheck.appendChild(iCheck);
-    divCheck.appendChild(aCheck);
-
+    if (text !== "") {
+        const li = document.createElement("li");
+        li.classList.add('row', 'mt-1', 'd-flex', 'border', 'border-5');
+        
+        const p = document.createElement("p");
+        p.classList.add('text-break', 'col-8', 'col-sm-9', 'mt-1');
+        p.textContent = text;
     
-    aTrash.appendChild(iTrash);
-    divTrash.appendChild(aTrash);
-
+        li.appendChild(p);
+        li.appendChild(addCheckBtn());
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
     
-    secondDiv.appendChild(p);
-    secondDiv.appendChild(divCheck);
-    secondDiv.appendChild(divTrash);
-
-    mainDiv.appendChild(secondDiv);
-
-
-    clearForm();
-    emptyMain();
-}
-
-function clearForm() {
-    divFather.children[0].value = ''
-}
-
-
-/* Empty main */
-
-function emptyMain() {
-    if(tasks.length < 0){
-        emptyTasks.classList.remove('d-none');
+        input.value = "";
+        empty.style.display = "none";
+      }
     }
-    if(tasks.length >= 0){
-        emptyTasks.classList.add('d-none');
+
+    function addCheckBtn(){
+        const checkBtn = document.createElement('a');
+
+        checkBtn.innerHTML = '<i class="check fa-solid fa-check bg-success"></i>';
+        checkBtn.classList.add('d-inline-block', 'col-1', 'mr-3', 'btn')
+
+        checkBtn.addEventListener('click', (e) => {
+            const item = e.target.parentElement.parentElement;
+
+            let textCompleted;
+            textCompleted = item.textContent  ;
+            item.textContent = 'COMPLETED TASK - ' + textCompleted;
+            item.classList.add('overflow-hidden')
+            checkBtn.style.display = 'none';
+
+            
+        } )
+        return checkBtn;
     }
-}
+
+    function addDeleteBtn() {
+        const deleteBtn = document.createElement("a");
+      
+        deleteBtn.innerHTML = "<i class='trash fa-solid fa-trash bg-danger'></i>";
+        deleteBtn.classList.add('d-inline-block', 'btn', 'col-1');
+      
+        deleteBtn.addEventListener("click", (e) => {
+          const item = e.target.parentElement.parentElement;
+          ul.removeChild(item);
+      
+          const items = document.querySelectorAll("li");
+      
+          if (items.length === 0) {
+            empty.style.display = "block";
+          }
+        });
+      
+        return deleteBtn;
+      }
 
 
-function limpiarHTML(){
-    mainDiv.innerHTML = '';
-}
-
-
-
-
-function deleteElement(e){
-    console.log('Eliminar')
-
-}
